0252a80727612c0aab6da9d827e522659e25ce01
Bibek Roy <106325261+Bibek478@users.noreply.github.com>
2025-04-14 09:36:40 +0000

    little adjustments

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-/*=========================== toggle icon navbar =================================*/
-
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('fa-xmark');
-    navbar.classList.toggle('active');
-}
-
-/*=========================== scroll section active link =================================*/
-
+/*==================== scroll sections active link ====================*/
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
@@ -20,34 +9,44 @@ window.onscroll = () => {
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if (top >= offset && top < offset + height) {
+        if(top >= offset && top < offset + height) {
             navLinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
         };
     });
-    /*=========================== sticky navbar =================================*/
-    let header = document.querySelector('header');
-    header.classList.toggle('sticky', window.scrollY > 100);
-
-    /*=========================== remove toggle icon navbar =================================*/
-    menuIcon.classList.remove('fa-xmark');
-    navbar.classList.remove('active');
 };
 
-/*=========================== scroll reveal =================================*/
-ScrollReveal({
-    distance: '80px',
-    duration: 2000,
-    delay: 200,
+/*==================== toggle icon navbar ====================*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+/*==================== typing animation ====================*/
+const typed = new Typed('.multiple-text', {
+    strings: ['Web Developer', 'B.Tech Student', 'AI/ML Enthusiast'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
 });
 
-ScrollReveal().reveal('.home-content, heading', { origin: 'top' });
-ScrollReveal().reveal('.home-img, .projects-container, .skills-box, .contact form', { origin: 'bottom' });
-ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
-ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+/*==================== scroll reveal ====================*/
+ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
 
-/*=========================== typed js =================================*/
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .projects-container, .skills-container, .contact-buttons', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1', { origin: 'left' });
+ScrollReveal().reveal('.home-content p', { origin: 'right' });
 
 
